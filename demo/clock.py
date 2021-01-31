da2a7b7df08ceca7749ee2bd284db99581b81fa1
Thom <xoxo@thomhayward.com>
2021-01-31 11:24:00 +0000

    Modifications for ATTiny85 with crude two-wire interface

--- demo/clock.py
+++ demo/clock.py
@@ -1,26 +1,74 @@
 #! /usr/bin/env python3
-
-import serial
+import RPi.GPIO as GPIO
 import time
 
-ser = serial.Serial(
-    port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-    baudrate = 38400,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    xonxoff=False,
-    rtscts=False,
-    dsrdtr=False,
-    timeout=1
-)
+def convert(value, bit):
+    if value & (1 << bit) == 0:
+        return GPIO.LOW
+    return GPIO.HIGH
 
+class SMBus(object):
+
+    pin_SCL = 0
+    pin_SDA = 0
+    delay = 000000
+
+    def __init__(self, bus=-1):
+        GPIO.setwarnings(True)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(12, GPIO.OUT)
+        GPIO.output(12, GPIO.HIGH)
+        time.sleep(0.33)
+        self.set_pin(23, 24)
+
+    def set_pin(self, SDA, SCL):
+        self.pin_SCL = SCL
+        self.pin_SDA = SDA
+        time.sleep(self.delay)
+
+    def start(self):
+        GPIO.setup([self.pin_SCL, self.pin_SDA], GPIO.OUT)
+        GPIO.output(self.pin_SDA, GPIO.HIGH)
+        GPIO.output(self.pin_SCL, GPIO.HIGH)
+        time.sleep(self.delay)
+        GPIO.output(self.pin_SDA, GPIO.LOW)
+        time.sleep(self.delay)
+
+    def stop(self):
+        GPIO.setup([self.pin_SCL, self.pin_SDA], GPIO.OUT)
+        GPIO.output(self.pin_SCL, GPIO.HIGH)
+        time.sleep(self.delay / 2)
+        GPIO.output(self.pin_SDA, GPIO.HIGH)
+        time.sleep(self.delay * 2)
+        GPIO.setup([self.pin_SCL, self.pin_SDA], GPIO.IN)
+
+    def send_byte(self, byte):
+        GPIO.setup([self.pin_SCL, self.pin_SDA], GPIO.OUT)
+        for i in range(0, 8):
+            GPIO.output(self.pin_SCL, GPIO.LOW)
+            GPIO.output(self.pin_SDA, convert(byte, 7 - i))
+            GPIO.output(self.pin_SCL, GPIO.HIGH)
+            time.sleep(self.delay)
+
+smb = SMBus()
 try:
+    tick = False
     while True:
-        ser.write(time.strftime('\x0D%H%M:').encode('ascii'))
-        time.sleep(1)
-        ser.write(time.strftime('\x0D%H%M').encode('ascii'))
+        smb.start()
+        now = time.strftime('%H%M')
+        tick = not tick
+        if tick:
+            now += ':'
+        else:
+            now += ' '
+        for byte in [ord(x) for x in now ]:
+            smb.send_byte(byte)
+        smb.stop()
         time.sleep(1)
 
 except KeyboardInterrupt:
-    ser.write(b'\x0D    ')
+    smb.start()
+    for byte in [ord(x) for x in '     ' ]:
+        smb.send_byte(byte)
+    smb.stop()
+    GPIO.cleanup()
--- driver.cc
+++ driver.cc
@@ -6,7 +6,7 @@
 
 // Define an architecture to appease vscode
 #ifndef __AVR_ARCH__
-#define __AVR_ATtiny84__
+#define __AVR_ATtiny85__
 #endif
 
 #ifndef F_CPU
@@ -15,142 +15,100 @@
 
 #include <avr/interrupt.h>
 #include <avr/io.h>
-#include <stdlib.h>
-#include <util/delay.h>
 #include "ascii.hh"
 
-#define LATCH_PIN PA0
-#define CLOCK_PIN PA1
-#define DATA_PIN PA2
+#define SN595_SRCLK_PIN PB1
+#define SN595_RCLK_PIN PB3
+#define SN595_SER_PIN PB4
 
-#define BAUDRATE            38400
-#define CYCLES_PER_BIT      (F_CPU / BAUDRATE)
-#if (CYCLES_PER_BIT > 255)
-#define     DIVISOR         8
-#define     PRESCALE        2
-#else
-#define     DIVISOR         1
-#define     PRESCALE        1
-#endif
-#define FULL_BIT_TICKS      (CYCLES_PER_BIT / DIVISOR)
-#define HALF_BIT_TICKS      (FULL_BIT_TICKS / 2)
-#define START_DELAY         42
-#define TIMER_START_DELAY   (START_DELAY / DIVISOR)
+#define USI_SDA PB0
+#define USI_SCL PB2
 
 #define DIGITS 5
 
 volatile uint8_t buffer_idx = 0;
-volatile uint8_t output[128];
+volatile uint8_t buffer[DIGITS];
+volatile uint8_t output[DIGITS];
 
-//static const uint8_t mask[] = { 0xfe, 0xfd, 0xfb, 0xf7, 0xef, 0xdf, 0xbf, 0x7f };
-static const uint8_t mask[] = { 0xFE, 0xEF, 0xF7, 0xFB, 0xFD };
+static const uint8_t mask[] = { 0xFE, 0xFD, 0xF7, 0xEF, 0xFB };
 
 template<int CLK_PIN, int DAT_PIN> void shift(uint8_t data) {
     for (int bit = 7; bit >= 0; bit--) {
+        PORTB &= ~_BV(CLK_PIN);
         if (((data >> bit) & 0x01) == 0x01) {
-            PORTA |= _BV(DAT_PIN);
+            PORTB |= _BV(DAT_PIN);
         } else {
-            PORTA &= ~_BV(DAT_PIN);
+            PORTB &= ~_BV(DAT_PIN);
         }
-        PORTA |= _BV(CLK_PIN);
-        PORTA &= ~_BV(CLK_PIN);
+        PORTB |= _BV(CLK_PIN);
     }
-}
-
-uint8_t reverse_byte(uint8_t x) {
-    x = ((x >> 1) & 0x55) | ((x << 1) & 0xaa);
-    x = ((x >> 2) & 0x33) | ((x << 2) & 0xcc);
-    x = ((x >> 4) & 0x0f) | ((x << 4) & 0xf0);
-    return x;    
 }
 
 int main() {
 
     cli();
 
-    // Setup rx
-    DDRA &= ~_BV(PA6);                                          // set PA6/DI as input
-    PORTA |= _BV(PA6);                                          // ... with internal pull-up
-    USICR = 0;                                                  // disable USI
-    GIMSK |=  _BV(PCIE0);                                       // enable pin change interrupts
-    PCMSK0 |= _BV(PCINT6);                                      // .. on PA6/DI
+    // Set the system clock prescaler to 1.
+    CLKPR = _BV(CLKPCE);
+    CLKPR = 0;
+
+    // Ensure display is blank on startup.
+    for (uint8_t i = 0; i < DIGITS; i++) {
+        output[i] = 0;
+    }
+
+    DDRB = _BV(SN595_SRCLK_PIN) | _BV(SN595_RCLK_PIN) | _BV(SN595_SER_PIN);
+
+    // Initialise the universal serial interface.
+    USISR = _BV(USISIF) | _BV(USIOIF) | _BV(USIPF) | _BV(USIDC);
+    USICR = _BV(USISIE) | _BV(USIWM1) | _BV(USICS1);
 
     sei();
-    
-    DDRA |= _BV(LATCH_PIN) | _BV(CLOCK_PIN) | _BV(DATA_PIN);
 
     for (;;) {
         // Ensure even illumination across all digits regardless of how many segments are active by only activating
         // one segment at a time.
         for (uint8_t segment = 0; segment < 8; segment++) {
             for (uint8_t digit = 0; digit < DIGITS; digit++) {
-                PORTA &= ~_BV(LATCH_PIN);
-                shift<CLOCK_PIN, DATA_PIN>(mask[digit]);
-                shift<CLOCK_PIN, DATA_PIN>(output[digit] & _BV(segment));
-                PORTA |= _BV(LATCH_PIN);
+                PORTB &= ~_BV(SN595_SRCLK_PIN);
+                shift<SN595_RCLK_PIN, SN595_SER_PIN>(mask[digit]);
+                shift<SN595_RCLK_PIN, SN595_SER_PIN>(output[digit] & _BV(segment));
+                PORTB |= _BV(SN595_SRCLK_PIN);
             }
         }
     }
 
 }
 
-ISR (PCINT0_vect) {                                         
-    if ((PINA & _BV(PA6)) == 0) {                               // PA6/DI has gone low; we've probably detected a start bit
-        GIMSK &= ~_BV(PCIE0);                                   // disable pin change interrupts
-        TCCR0A = _BV(WGM01);                                    // setup timer0 in CTC mode
-#if (DIVISOR == 8)
-        TCCR0B = _BV(CS01);                                     // set prescaler to cpu_freq/8
-#endif
-#if (DIVISOR == 1)
-        TCCR0B = _BV(CS00);                                     // set prescaler to cpu_freq/1
-#endif
-        GTCCR |= _BV(PSR10);                                    // reset prescaler
-        TCNT0 = 0;                                              // count up from 0
-        OCR0A = HALF_BIT_TICKS - TIMER_START_DELAY;             // try to trigger the timer in the middle of a signal
-        TIFR0 = _BV(OCF0A);                                     // clear output compare interrupt flag
-        TIMSK0 |= _BV(OCIE0A);                                  // enable output compare interrupt
+#define COPY_BUFFER() {\
+    buffer_idx = 0;\
+    for (uint8_t i = 0; i < DIGITS; i++) {\
+        output[i] = pgm_read_byte(&ascii_mappings[buffer[i]]);\
+    }\
+}
+
+ISR(USI_START_vect) {
+    if ((USISR & _BV(USIPF)) == _BV(USIPF) || buffer_idx == DIGITS) {
+        // Stop condition has occurred. Copy buffer to output.
+        COPY_BUFFER();
     }
+    buffer_idx = 0;
+    USISR = _BV(USISIF) | _BV(USIPF) | (0 << USICNT0);
+    USICR = _BV(USISIE) | _BV(USIOIE) | _BV(USIWM1) | _BV(USICS1);
 }
 
-ISR (TIM0_COMPA_vect) {
-    TIMSK0 &= ~_BV(OCIE0A);                                     // disable COMPA interrupt
-    TCNT0 = 0;                                                  // count up from 0
-    OCR0A = FULL_BIT_TICKS;                                     // shift every bit width
-    USICR = _BV(USIOIE) | _BV(USICS0);
-    USISR = _BV(USIOIF) | 0x08;
-}
-
-ISR (USI_OVF_vect) {                                            // USI has read eight bits
-    uint8_t data = reverse_byte(USIDR);
-    switch (data) {
-        case 0x08:  // BS
-            if (buffer_idx > 1) {
-                buffer_idx -= 1;
-            }
-            break;
-        case 0x0D:  // CR, Carriage Return
-            buffer_idx = 1;
-            output[0] = 0;
-            break;
-        case 0x3A:  // ':'
-            output[0] = 0x03;
-            break;
-        case 0x2E:  // '.'
-            if (buffer_idx > 1) {
-                output[buffer_idx - 1] |= 0x80;
-            }
-            break;
-        default:
-            uint8_t mapped = pgm_read_byte(&ascii_mappings[data]);
-            output[buffer_idx] = mapped;
-            buffer_idx += 1;
-            if (buffer_idx > 128) {
-                buffer_idx = 0;
-            }
-            break;
-
-    }    
-    USICR = 0;                                                  // disable USI; we've finished reading data
-    GIFR = _BV(PCIF0);                                          // clear pin change interrupt flag
-    GIMSK |= _BV(PCIE0);                                        // enable pin change interrupts again
+ISR(USI_OVF_vect) {
+    if (buffer_idx < DIGITS) {
+        buffer[buffer_idx] = USIBR;
+        buffer_idx += 1;
+    }
+    if ((USISR & _BV(USIPF)) == _BV(USIPF) || buffer_idx == DIGITS) {
+        // Stop condition has occurred. Copy buffer to output.
+        COPY_BUFFER();
+        // Reset the USI.
+        USISR = _BV(USISIF) | _BV(USIOIF) | _BV(USIPF) | _BV(USIDC) | (0 << USICNT0);
+        USICR = _BV(USISIE) | _BV(USIWM1) | _BV(USICS1);
+        return;
+    }
+    USISR = _BV(USIOIF) | (0 << USICNT0);
 }
